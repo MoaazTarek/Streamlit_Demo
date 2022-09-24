faab2a2a9783a2e04ed162bbc941ad533c3feaba
MoaazTarek <52607626+MoaazTarek@users.noreply.github.com>
2022-09-24 20:13:48 +0200

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import os
+
+os.chdir('D:/Moaaz/Programming/Epsilon_Data_Science_Diploma/17_Streamlit/Streamlit_Demo')
+
+st.title('Hello World')
+
+st.write('this is a test')
+
+st.markdown(''' 
+                * This is a test
+                * This is a test
+                * This is a test ''')
+
+st.markdown(''' 
+                1-  This is a test
+                
+                2-  This is a test
+                
+                3-  This is a test   ''')                
