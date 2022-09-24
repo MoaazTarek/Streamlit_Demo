9c1b444b623b663c0df477054fae3023bd4abab6
MoaazTarek <52607626+MoaazTarek@users.noreply.github.com>
2022-09-24 20:36:48 +0200

    Update Data.py

--- pages/Data.py
+++ pages/Data.py
@@ -1,6 +1,6 @@
 import os
 
-os.chdir('D:/Moaaz/Programming/Epsilon_Data_Science_Diploma/17_Streamlit/Streamlit_Demo/Pages')
+#os.chdir('D:/Moaaz/Programming/Epsilon_Data_Science_Diploma/17_Streamlit/Streamlit_Demo/Pages')
 
 import About  
 import pandas as pd
